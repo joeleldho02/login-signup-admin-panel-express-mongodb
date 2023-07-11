79ef19a991038d57573068c0bdac1f6297ff4e78
Joel Eldho <33107471+joeleldho02@users.noreply.github.com>
2023-07-11 09:58:08 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const logger = require('morgan');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const { v4:uuidv4 } = require('uuid');
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080;
+
+//log requests
+app.use(logger('dev'));
+
+//parse request to body-parser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+//set view engine
+app.set("view enigine", "hbs")
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
+app.use("/js", express.static(path.join(__dirname, "public/js")));
+app.use(session({
+    secret: uuidv4(), 
+    cookie: {maxAge:600000},
+    saveUninitialized: true
+}))
+
+
+
+
+app.get('/', function(req, res){
+    res.render('login');
+})
+app.get('/login', function(req, res){
+    res.render('login');
+})
+app.get('/signup', function(req, res){
+    res.render('signup.hbs');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`);
+})
+
+module.exports = app;
